3e9249aa19033d95c7b6470d4d3bf7bd1b67fee3
Cheewbacca <vanya.timoschenko85@gmail.com>
2020-07-14 10:53:24 +0300

    js fixes

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,20 @@
+/*global window */
+
 'use strict';
 
-window.onload = function(){
-    const burger = document.getElementById("burger");
-    const hiddenMenu = document.getElementsByTagName("nav")[0];
+
+
+const _window = window;
+
+_window.onload = function(){
+    const burger = _window.document.getElementById("burger");
+    const hiddenMenu = _window.document.getElementsByTagName("nav")[0];
 
     var isClicked = true;
 
     burger.addEventListener("click", (e) => {
         e.preventDefault(); 
-        let children = document.getElementById("burger").children;
+        let children = _window.document.getElementById("burger").children;
         if (isClicked){
             children[0].style.transform = 'rotate(-45deg) translate(2px)';
             children[1].style.transform = 'rotate(45deg) translate(2px)';
@@ -22,44 +28,46 @@ window.onload = function(){
         isClicked = !isClicked;
     });
 
-    const header = document.getElementById("header");
-    const how_it_works_section_pos = document.getElementById("how_it_works").offsetTop;
+    const header = _window.document.getElementById("header");
+    const how_it_works_section_pos = _window.document.getElementById("how_it_works").offsetTop;
 
-    const how_it_works_h2 = document.getElementById("how_it_works_h2");
-    const how_it_works_p = document.getElementById("how_it_works_p");
+    const how_it_works_h2 = _window.document.getElementById("how_it_works_h2");
+    const how_it_works_p = _window.document.getElementById("how_it_works_p");
 
-    const steps = document.getElementsByClassName("step");
+    const steps = _window.document.getElementsByClassName("step");
     const steps_top_pos = steps[0].offsetTop;
     const steps_bottom_pos = steps[1].offsetTop;
 
-    const footer_height = document.getElementById("footer").offsetHeight;
-    const last_section = document.getElementById("last_section");
+    const footer_height = _window.document.getElementById("footer").offsetHeight;
+    const last_section = _window.document.getElementById("last_section");
 
     last_section.style.marginBottom = footer_height + "px";
 
-    window.addEventListener("scroll", () => {
+    _window.addEventListener("scroll", () => {
         var lastScrollTop = 0;
-        let state = window.pageYOffset || document.documentElement.scrollTop;
+        let state = _window.pageYOffset || _window.document.documentElement.scrollTop;
 
-        if (state > lastScrollTop) {
-            header.style.transform = "translate(0, -80%)";
-        } else {
-            header.style.transform = "translate(0, 0)";
+        if (parseInt( _window.innerWidth ) >= 500) {
+            if (state > lastScrollTop) {
+                header.style.transform = "translate(0, -80%)";
+            } else {
+                header.style.transform = "translate(0, 0)";
+            }
         }
 
         lastScrollTop = state <= 0 ? 0 : state;
 
-        if (window.pageYOffset > how_it_works_section_pos - document.documentElement.clientHeight / 2){
+        if (_window.pageYOffset > how_it_works_section_pos - _window.document.documentElement.clientHeight / 2){
             how_it_works_h2.style.transform = "translate(0)";
             how_it_works_p.style.transform = "translate(0)";
         }
 
-        if (window.pageYOffset > steps_top_pos - document.documentElement.clientHeight / 2){
+        if (_window.pageYOffset > steps_top_pos - _window.document.documentElement.clientHeight / 2){
             steps[0].style.opacity = 1;
             steps[2].style.opacity = 1;
         }
 
-        if (window.pageYOffset > steps_bottom_pos - document.documentElement.clientHeight / 2){
+        if (_window.pageYOffset > steps_bottom_pos - _window.document.documentElement.clientHeight / 2){
             steps[1].style.opacity = 1;
         }
 
@@ -70,13 +78,13 @@ window.onload = function(){
     });
 
     header.addEventListener("mouseout", () => {
-        if ( document.documentElement.scrollTop > 100){
+        if ( _window.document.documentElement.scrollTop > 100){
             header.style.transform = "translate(0, -80%)";
         } 
     });
 
-    const preloader = document.getElementById("preloader");
+    const preloader = _window.document.getElementById("preloader");
 
-    setTimeout( () => preloader.style.display = "none", 2000 );
+    let show_content = _window.setTimeout( () => preloader.style.display = "none", 2000 );
 };
 
